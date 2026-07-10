7db30e629aa6586dcb991a17920f6bdd6a74ef07
(Sherline Bertrand) <sherlinebertrand@gmail.com>
2026-07-09 23:54:23 -0400

    comments and print function

---
+++ app.py
@@ -0,0 +1,17 @@
+# This is a single-lined comment
+
+# This is multiline comment
+# 1. create User Name
+
+# Store the final exam score
+x = 10
+
+x = 9 # Final exam score
+
+print("Hi Python")
+print('Hi Python')
+# Print ("Hi')
+
+print("--------------------------------")
+print("Hi, this is my first python code")
+print("--------------------------------")
