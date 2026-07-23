3edc620edd201fb75115baa84cea1737d3de9cc9
(Sherline Bertrand) <sherlinebertrand@gmail.com>
2026-07-22 23:53:02 -0400

    data structures, list

---
+++ 07_data_structures/12_data_structures.py
@@ -0,0 +1,18 @@
+# ================================================================================
+# Data Structures Behavior
+# ----------------------------------------
+# Python provides multiple built-in collection types.
+# Each one has different behavior regarding:
+# - Order
+# - Mutability
+# - Duplicates
+# - Indexing
+# ================================================================================
+
+
+# ---------------------------------------
+# List
+# ---------------------------------------
+# Lists are: Ordered, Mutable, Allow duplicates, Indexed
+
+my_list = [10, 30, 20, 10]
